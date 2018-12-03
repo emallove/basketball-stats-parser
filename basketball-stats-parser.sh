c529b7c0ce4a084d43fe20eba548a141ccc786aa
Ethan Mallove <mallove@fas.harvard.edu>
2018-12-03 13:58:39 -0500

    Simple basketball stats parser

---
+++ basketball-stats-parser.sh
@@ -0,0 +1,573 @@
+#!/bin/sh
+
+# sh -x stats-parser.sh 
+
+file="stats-file.txt"
+
+for variable in words; do
+
+  #   b    Bdzpytp Rqhh
+  echo Bzbevxe Rucc 2P
+  ack --all '^b.*[M]\b' $file | wc -l
+  #   v    Cjvoux Pjrjx
+  echo Cxtshe Px7xe 2P
+  ack --xttfile | wc -l
+  #   it Jrom Ljxfkjs
+  echo Jgvb Lxelbxz 2P
+  ack --xttfile | wc -l
+  #   iv Jeck Dknuqyjkvud
+  echo Juoe Db9haoxbthc 2P
+  ack --xttfile | wc -l
+  #   q Jpjlqek Pp7pk Pjrjx
+  echo Jxaqcue Px7xe 2P
+  ack --xttfile | wc -l
+  #   i9 Jrwypk Mjvvcok
+  echo Jgthxe Mxttusb 2P
+  ack --xttfile | wc -l
+  #   9 Mphhcmx Hr He
+  echo Mxddobn Hg 2P
+  ack --xttfile | wc -l
+  #   e Nmo Kgpvh Ksjfw
+  echo Nbv Kzxld 2P
+  ack --xttfile | wc -l
+  #   u Oxmk Shmqkmf Swkdxki
+  echo Onbe Sdbcebf 2P
+  ack --xttfile | wc -l
+  #   aw Cekeg Bskxxjx
+  echo Cueuz Bzbeexe 2P
+  ack --xttfile | wc -l
+  #   c Segmk Pphmw Pjwkv
+  echo Suzbe Pxdbt 2P
+  ack --xttfile | wc -l
+  #   d Tce9pq Lp9imgh Ljnbksw
+  echo Tou9xc Lx9ybzd 2P
+  ack --xttfile | wc -l
+  #   n7 Wyww Guxhkv
+  echo Whtt Ghevbt 2P
+  ack --xttfile | wc -l
+
+  #   w Bgmkopk Reqq Rcdd
+  echo Bzbevxe Rucc 2PA
+  ack --xttfile | wc -l
+  #   s Cpwdyk Pp7pk Pjrjx
+  echo Cxtshe Px7xe 2PA
+  ack --xttfile | wc -l
+  #   it Jrom Ljxfkjs
+  echo Jgvb Lxelbxz 2PA
+  ack --xttfile | wc -l
+  #   iv Jeck Dknuqyjkvud
+  echo Juoe Db9haoxbthc 2PA
+  ack --xttfile | wc -l
+  #   q Jpjlqek Pp7pk Pjrjx
+  echo Jxaqcue Px7xe 2PA
+  ack --xttfile | wc -l
+  #   i9 Jrwypk Mjvvcok
+  echo Jgthxe Mxttusb 2PA
+  ack --xttfile | wc -l
+  #   9 Mphhcmx Hr He
+  echo Mxddobn Hg 2PA
+  ack --xttfile | wc -l
+  #   e Nmo Kgpvh Ksjfw
+  echo Nbv Kzxld 2PA
+  ack --xttfile | wc -l
+  #   u Oxmk Shmqkmf Swkdxki
+  echo Onbe Sdbcebf 2PA
+  ack --xttfile | wc -l
+  #   aw Cekeg Bskxxjx
+  echo Cueuz Bzbeexe 2PA
+  ack --xttfile | wc -l
+  #   c Segmk Pphmw Pjwkv
+  echo Suzbe Pxdbt 2PA
+  ack --xttfile | wc -l
+  #   d Tce9pq Lp9imgh Ljnbksw
+  echo Tou9xc Lx9ybzd 2PA
+  ack --xttfile | wc -l
+  #   n7 Wyww Guxhkv
+  echo Whtt Ghevbt 2PA
+  ack --xttfile | wc -l
+
+  #   w Bgmkopk Reqq Rcdd
+  echo Bzbevxe Rucc 3P
+  ack --xttfile | wc -l
+  #   s Cpwdyk Pp7pk Pjrjx
+  echo Cxtshe Px7xe 3P
+  ack --xttfile | wc -l
+  #   it Jrom Ljxfkjs
+  echo Jgvb Lxelbxz 3P
+  ack --xttfile | wc -l
+  #   iv Jeck Dknuqyjkvud
+  echo Juoe Db9haoxbthc 3P
+  ack --xttfile | wc -l
+  #   q Jpjlqek Pp7pk Pjrjx
+  echo Jxaqcue Px7xe 3P
+  ack --xttfile | wc -l
+  #   i9 Jrwypk Mjvvcok
+  echo Jgthxe Mxttusb 3P
+  ack --xttfile | wc -l
+  #   9 Mphhcmx Hr He
+  echo Mxddobn Hg 3P
+  ack --xttfile | wc -l
+  #   e Nmo Kgpvh Ksjfw
+  echo Nbv Kzxld 3P
+  ack --xttfile | wc -l
+  #   u Oxmk Shmqkmf Swkdxki
+  echo Onbe Sdbcebf 3P
+  ack --xttfile | wc -l
+  #   aw Cekeg Bskxxjx
+  echo Cueuz Bzbeexe 3P
+  ack --xttfile | wc -l
+  #   c Segmk Pphmw Pjwkv
+  echo Suzbe Pxdbt 3P
+  ack --xttfile | wc -l
+  #   d Tce9pq Lp9imgh Ljnbksw
+  echo Tou9xc Lx9ybzd 3P
+  ack --xttfile | wc -l
+  #   n7 Wyww Guxhkv
+  echo Whtt Ghevbt 3P
+  ack --xttfile | wc -l
+
+
+  #   w Bgmkopk Reqq Rcdd
+  echo Bzbevxe Rucc 3PA
+  ack --xttfile | wc -l
+  #   s Cpwdyk Pp7pk Pjrjx
+  echo Cxtshe Px7xe 3PA
+  ack --xttfile | wc -l
+  #   it Jrom Ljxfkjs
+  echo Jgvb Lxelbxz 3PA
+  ack --xttfile | wc -l
+  #   iv Jeck Dknuqyjkvud
+  echo Juoe Db9haoxbthc 3PA
+  ack --xttfile | wc -l
+  #   q Jpjlqek Pp7pk Pjrjx
+  echo Jxaqcue Px7xe 3PA
+  ack --xttfile | wc -l
+  #   i9 Jrwypk Mjvvcok
+  echo Jgthxe Mxttusb 3PA
+  ack --xttfile | wc -l
+  #   9 Mphhcmx Hr He
+  echo Mxddobn Hg 3PA
+  ack --xttfile | wc -l
+  #   e Nmo Kgpvh Ksjfw
+  echo Nbv Kzxld 3PA
+  ack --xttfile | wc -l
+  #   u Oxmk Shmqkmf Swkdxki
+  echo Onbe Sdbcebf 3PA
+  ack --xttfile | wc -l
+  #   aw Cekeg Bskxxjx
+  echo Cueuz Bzbeexe 3PA
+  ack --xttfile | wc -l
+  #   c Segmk Pphmw Pjwkv
+  echo Suzbe Pxdbt 3PA
+  ack --xttfile | wc -l
+  #   d Tce9pq Lp9imgh Ljnbksw
+  echo Tou9xc Lx9ybzd 3PA
+  ack --xttfile | wc -l
+  #   n7 Wyww Guxhkv
+  echo Whtt Ghevbt 3PA
+  ack --xttfile | wc -l
+
+  #   w Bgmkopk Reqq Rcdd
+  echo Bzbevxe Rucc FT
+  ack --xttfile | wc -l
+  #   s Cpwdyk Pp7pk Pjrjx
+  echo Cxtshe Px7xe FT
+  ack --xttfile | wc -l
+  #   it Jrom Ljxfkjs
+  echo Jgvb Lxelbxz FT
+  ack --xttfile | wc -l
+  #   iv Jeck Dknuqyjkvud
+  echo Juoe Db9haoxbthc FT
+  ack --xttfile | wc -l
+  #   q Jpjlqek Pp7pk Pjrjx
+  echo Jxaqcue Px7xe FT
+  ack --xttfile | wc -l
+  #   i9 Jrwypk Mjvvcok
+  echo Jgthxe Mxttusb FT
+  ack --xttfile | wc -l
+  #   9 Mphhcmx Hr He
+  echo Mxddobn Hg FT
+  ack --xttfile | wc -l
+  #   e Nmo Kgpvh Ksjfw
+  echo Nbv Kzxld FT
+  ack --xttfile | wc -l
+  #   u Oxmk Shmqkmf Swkdxki
+  echo Onbe Sdbcebf FT
+  ack --xttfile | wc -l
+  #   aw Cekeg Bskxxjx
+  echo Cueuz Bzbeexe FT
+  ack --xttfile | wc -l
+  #   c Segmk Pphmw Pjwkv
+  echo Suzbe Pxdbt FT
+  ack --xttfile | wc -l
+  #   d Tce9pq Lp9imgh Ljnbksw
+  echo Tou9xc Lx9ybzd FT
+  ack --xttfile | wc -l
+  #   n7 Wyww Guxhkv
+  echo Whtt Ghevbt FT
+  ack --xttfile | wc -l
+
+
+  #   w Bgmkopk Reqq Rcdd
+  echo Bzbevxe Rucc FTA
+  ack --xttfile | wc -l
+  #   s Cpwdyk Pp7pk Pjrjx
+  echo Cxtshe Px7xe FTA
+  ack --xttfile | wc -l
+  #   it Jrom Ljxfkjs
+  echo Jgvb Lxelbxz FTA
+  ack --xttfile | wc -l
+  #   iv Jeck Dknuqyjkvud
+  echo Juoe Db9haoxbthc FTA
+  ack --xttfile | wc -l
+  #   q Jpjlqek Pp7pk Pjrjx
+  echo Jxaqcue Px7xe FTA
+  ack --xttfile | wc -l
+  #   i9 Jrwypk Mjvvcok
+  echo Jgthxe Mxttusb FTA
+  ack --xttfile | wc -l
+  #   9 Mphhcmx Hr He
+  echo Mxddobn Hg FTA
+  ack --xttfile | wc -l
+  #   e Nmo Kgpvh Ksjfw
+  echo Nbv Kzxld FTA
+  ack --xttfile | wc -l
+  #   u Oxmk Shmqkmf Swkdxki
+  echo Onbe Sdbcebf FTA
+  ack --xttfile | wc -l
+  #   aw Cekeg Bskxxjx
+  echo Cueuz Bzbeexe FTA
+  ack --xttfile | wc -l
+  #   c Segmk Pphmw Pjwkv
+  echo Suzbe Pxdbt FTA
+  ack --xttfile | wc -l
+  #   d Tce9pq Lp9imgh Ljnbksw
+  echo Tou9xc Lx9ybzd FTA
+  ack --xttfile | wc -l
+  #   n7 Wyww Guxhkv
+  echo Whtt Ghevbt FTA
+  ack --xttfile | wc -l
+
+
+  #   w Bgmkopk Reqq Rcdd
+  echo Bzbevxe Rucc ORB
+  ack --xttfile | wc -l
+  #   s Cpwdyk Pp7pk Pjrjx
+  echo Cxtshe Px7xe ORB
+  ack --xttfile | wc -l
+  #   it Jrom Ljxfkjs
+  echo Jgvb Lxelbxz ORB
+  ack --xttfile | wc -l
+  #   iv Jeck Dknuqyjkvud
+  echo Juoe Db9haoxbthc ORB
+  ack --xttfile | wc -l
+  #   q Jpjlqek Pp7pk Pjrjx
+  echo Jxaqcue Px7xe ORB
+  ack --xttfile | wc -l
+  #   i9 Jrwypk Mjvvcok
+  echo Jgthxe Mxttusb ORB
+  ack --xttfile | wc -l
+  #   9 Mphhcmx Hr He
+  echo Mxddobn Hg ORB
+  ack --xttfile | wc -l
+  #   e Nmo Kgpvh Ksjfw
+  echo Nbv Kzxld ORB
+  ack --xttfile | wc -l
+  #   u Oxmk Shmqkmf Swkdxki
+  echo Onbe Sdbcebf ORB
+  ack --xttfile | wc -l
+  #   aw Cekeg Bskxxjx
+  echo Cueuz Bzbeexe ORB
+  ack --xttfile | wc -l
+  #   c Segmk Pphmw Pjwkv
+  echo Suzbe Pxdbt ORB
+  ack --xttfile | wc -l
+  #   d Tce9pq Lp9imgh Ljnbksw
+  echo Tou9xc Lx9ybzd ORB
+  ack --xttfile | wc -l
+  #   n7 Wyww Guxhkv
+  echo Whtt Ghevbt ORB
+  ack --xttfile | wc -l
+
+  #   w Bgmkopk Reqq Rcdd
+  echo Bzbevxe Rucc DRB
+  ack --xttfile | wc -l
+  #   s Cpwdyk Pp7pk Pjrjx
+  echo Cxtshe Px7xe DRB
+  ack --xttfile | wc -l
+  #   it Jrom Ljxfkjs
+  echo Jgvb Lxelbxz DRB
+  ack --xttfile | wc -l
+  #   iv Jeck Dknuqyjkvud
+  echo Juoe Db9haoxbthc DRB
+  ack --xttfile | wc -l
+  #   q Jpjlqek Pp7pk Pjrjx
+  echo Jxaqcue Px7xe DRB
+  ack --xttfile | wc -l
+  #   i9 Jrwypk Mjvvcok
+  echo Jgthxe Mxttusb DRB
+  ack --xttfile | wc -l
+  #   9 Mphhcmx Hr He
+  echo Mxddobn Hg DRB
+  ack --xttfile | wc -l
+  #   e Nmo Kgpvh Ksjfw
+  echo Nbv Kzxld DRB
+  ack --xttfile | wc -l
+  #   u Oxmk Shmqkmf Swkdxki
+  echo Onbe Sdbcebf DRB
+  ack --xttfile | wc -l
+  #   aw Cekeg Bskxxjx
+  echo Cueuz Bzbeexe DRB
+  ack --xttfile | wc -l
+  #   c Segmk Pphmw Pjwkv
+  echo Suzbe Pxdbt DRB
+  ack --xttfile | wc -l
+  #   d Tce9pq Lp9imgh Ljnbksw
+  echo Tou9xc Lx9ybzd DRB
+  ack --xttfile | wc -l
+  #   n7 Wyww Guxhkv
+  echo Whtt Ghevbt DRB
+  ack --xttfile | wc -l
+
+  #   w Bgmkopk Reqq Rcdd
+  echo Bzbevxe Rucc AST
+  ack --xttfile | wc -l
+  #   s Cpwdyk Pp7pk Pjrjx
+  echo Cxtshe Px7xe AST
+  ack --xttfile | wc -l
+  #   it Jrom Ljxfkjs
+  echo Jgvb Lxelbxz AST
+  ack --xttfile | wc -l
+  #   iv Jeck Dknuqyjkvud
+  echo Juoe Db9haoxbthc AST
+  ack --xttfile | wc -l
+  #   q Jpjlqek Pp7pk Pjrjx
+  echo Jxaqcue Px7xe AST
+  ack --xttfile | wc -l
+  #   i9 Jrwypk Mjvvcok
+  echo Jgthxe Mxttusb AST
+  ack --xttfile | wc -l
+  #   9 Mphhcmx Hr He
+  echo Mxddobn Hg AST
+  ack --xttfile | wc -l
+  #   e Nmo Kgpvh Ksjfw
+  echo Nbv Kzxld AST
+  ack --xttfile | wc -l
+  #   u Oxmk Shmqkmf Swkdxki
+  echo Onbe Sdbcebf AST
+  ack --xttfile | wc -l
+  #   aw Cekeg Bskxxjx
+  echo Cueuz Bzbeexe AST
+  ack --xttfile | wc -l
+  #   c Segmk Pphmw Pjwkv
+  echo Suzbe Pxdbt AST
+  ack --xttfile | wc -l
+  #   d Tce9pq Lp9imgh Ljnbksw
+  echo Tou9xc Lx9ybzd AST
+  ack --xttfile | wc -l
+  #   n7 Wyww Guxhkv
+  echo Whtt Ghevbt AST
+  ack --xttfile | wc -l
+
+
+  #   w Bgmkopk Reqq Rcdd
+  echo Bzbevxe Rucc STL
+  ack --xttfile | wc -l
+  #   s Cpwdyk Pp7pk Pjrjx
+  echo Cxtshe Px7xe STL
+  ack --xttfile | wc -l
+  #   it Jrom Ljxfkjs
+  echo Jgvb Lxelbxz STL
+  ack --xttfile | wc -l
+  #   iv Jeck Dknuqyjkvud
+  echo Juoe Db9haoxbthc STL
+  ack --xttfile | wc -l
+  #   q Jpjlqek Pp7pk Pjrjx
+  echo Jxaqcue Px7xe STL
+  ack --xttfile | wc -l
+  #   i9 Jrwypk Mjvvcok
+  echo Jgthxe Mxttusb STL
+  ack --xttfile | wc -l
+  #   9 Mphhcmx Hr He
+  echo Mxddobn Hg STL
+  ack --xttfile | wc -l
+  #   e Nmo Kgpvh Ksjfw
+  echo Nbv Kzxld STL
+  ack --xttfile | wc -l
+  #   u Oxmk Shmqkmf Swkdxki
+  echo Onbe Sdbcebf STL
+  ack --xttfile | wc -l
+  #   aw Cekeg Bskxxjx
+  echo Cueuz Bzbeexe STL
+  ack --xttfile | wc -l
+  #   c Segmk Pphmw Pjwkv
+  echo Suzbe Pxdbt STL
+  ack --xttfile | wc -l
+  #   d Tce9pq Lp9imgh Ljnbksw
+  echo Tou9xc Lx9ybzd STL
+  ack --xttfile | wc -l
+  #   n7 Wyww Guxhkv
+  echo Whtt Ghevbt STL
+  ack --xttfile | wc -l
+
+  #   w Bgmkopk Reqq Rcdd
+  echo Bzbevxe Rucc BLK
+  ack --xttfile | wc -l
+  #   s Cpwdyk Pp7pk Pjrjx
+  echo Cxtshe Px7xe BLK
+  ack --xttfile | wc -l
+  #   it Jrom Ljxfkjs
+  echo Jgvb Lxelbxz BLK
+  ack --xttfile | wc -l
+  #   iv Jeck Dknuqyjkvud
+  echo Juoe Db9haoxbthc BLK
+  ack --xttfile | wc -l
+  #   q Jpjlqek Pp7pk Pjrjx
+  echo Jxaqcue Px7xe BLK
+  ack --xttfile | wc -l
+  #   i9 Jrwypk Mjvvcok
+  echo Jgthxe Mxttusb BLK
+  ack --xttfile | wc -l
+  #   9 Mphhcmx Hr He
+  echo Mxddobn Hg BLK
+  ack --xttfile | wc -l
+  #   e Nmo Kgpvh Ksjfw
+  echo Nbv Kzxld BLK
+  ack --xttfile | wc -l
+  #   u Oxmk Shmqkmf Swkdxki
+  echo Onbe Sdbcebf BLK
+  ack --xttfile | wc -l
+  #   aw Cekeg Bskxxjx
+  echo Cueuz Bzbeexe BLK
+  ack --xttfile | wc -l
+  #   c Segmk Pphmw Pjwkv
+  echo Suzbe Pxdbt BLK
+  ack --xttfile | wc -l
+  #   w Tce9pq Lp9imgh Ljnbksw
+  echo Tou9xc Lx9ybzd BLK
+  ack --xttfile | wc -l
+  #   n7 Wyww Guxhkv
+  echo Whtt Ghevbt BLK
+  ack --xttfile | wc -l
+
+  #   w Bgmkopk Reqq Rcdd
+  echo Bzbevxe Rucc TOV
+  ack --xttfile | wc -l
+  #   s Cpwdyk Pp7pk Pjrjx
+  echo Cxtshe Px7xe TOV
+  ack --xttfile | wc -l
+  #   it Jrom Ljxfkjs
+  echo Jgvb Lxelbxz TOV
+  ack --xttfile | wc -l
+  #   iv Jeck Dknuqyjkvud
+  echo Juoe Db9haoxbthc TOV
+  ack --xttfile | wc -l
+  #   q Jpjlqek Pp7pk Pjrjx
+  echo Jxaqcue Px7xe TOV
+  ack --xttfile | wc -l
+  #   i9 Jrwypk Mjvvcok
+  echo Jgthxe Mxttusb TOV
+  ack --xttfile | wc -l
+  #   9 Mphhcmx Hr He
+  echo Mxddobn Hg TOV
+  ack --xttfile | wc -l
+  #   e Nmo Kgpvh Ksjfw
+  echo Nbv Kzxld TOV
+  ack --xttfile | wc -l
+  #   u Oxmk Shmqkmf Swkdxki
+  echo Onbe Sdbcebf TOV
+  ack --xttfile | wc -l
+  #   aw Cekeg Bskxxjx
+  echo Cueuz Bzbeexe TOV
+  ack --xttfile | wc -l
+  #   c Segmk Pphmw Pjwkv
+  echo Suzbe Pxdbt TOV
+  ack --xttfile | wc -l
+  #   d Tce9pq Lp9imgh Ljnbksw
+  echo Tou9xc Lx9ybzd TOV
+  ack --xttfile | wc -l
+  #   n7 Wyww Guxhkv
+  echo Whtt Ghevbt TOV
+  ack --xttfile | wc -l
+
+  #   w Bgmkopk Reqq Rcdd
+  echo Bzbevxe Rucc PF
+  ack --xttfile | wc -l
+  #   s Cpwdyk Pp7pk Pjrjx
+  echo Cxtshe Px7xe PF
+  ack --xttfile | wc -l
+  #   it Jrom Ljxfkjs
+  echo Jgvb Lxelbxz PF
+  ack --xttfile | wc -l
+  #   iv Jeck Dknuqyjkvud
+  echo Juoe Db9haoxbthc PF
+  ack --xttfile | wc -l
+  #   q Jpjlqek Pp7pk Pjrjx
+  echo Jxaqcue Px7xe PF
+  ack --xttfile | wc -l
+  #   i9 Jrwypk Mjvvcok
+  echo Jgthxe Mxttusb PF
+  ack --xttfile | wc -l
+  #   9 Mphhcmx Hr He
+  echo Mxddobn Hg PF
+  ack --xttfile | wc -l
+  #   e Nmo Kgpvh Ksjfw
+  echo Nbv Kzxld PF
+  ack --xttfile | wc -l
+  #   u Oxmk Shmqkmf Swkdxki
+  echo Onbe Sdbcebf PF
+  ack --xttfile | wc -l
+  #   aw Cekeg Bskxxjx
+  echo Cueuz Bzbeexe PF
+  ack --xttfile | wc -l
+  #   c Segmk Pphmw Pjwkv
+  echo Suzbe Pxdbt PF
+  ack --xttfile | wc -l
+  #   d Tce9pq Lp9imgh Ljnbksw
+  echo Tou9xc Lx9ybzd PF
+  ack --xttfile | wc -l
+  #   n7 Wyww Guxhkv
+  echo Whtt Ghevbt PF
+  ack --xttfile | wc -l
+
+  #   ? uyyuebed baou opponent
+  echo uyyuebed 2P 2P
+  ack --xttfile | wc -l
+  #   ? uyyuebed opponent
+  echo uyyuebed 2PA 2PA
+  ack --xttfile | wc -l
+  #   ? uyyuebed opponent
+  echo uyyuebed 3P 3P
+  ack --xttfile | wc -l
+  #   ? uyyuebed opponent
+  echo uyyuebed 3PA 3PA
+  ack --xttfile | wc -l
+  #   ? uyyuebed opponent
+  echo uyyuebed FT FT
+  ack --xttfile | wc -l
+  #   ? uyyuebed opponent
+  echo uyyuebed FTA FTA
+  ack --xttfile | wc -l
+  #   ? uyyuebed opponent
+  echo uyyuebed ORB ORB
+  ack --xttfile | wc -l
+  #   ? uyyuebed opponent
+  echo uyyuebed DRB DRB
+  ack --xttfile | wc -l
+  #   ? uyyuebed opponent
+  echo uyyuebed AST AST
+  ack --xttfile | wc -l
+  #   ? uyyuebed opponent
+  echo uyyuebed STL STL
+  ack --xttfile | wc -l
+  #   ? uyyuebed opponent
+  echo uyyuebed BLK BLK
+  ack --xttfile | wc -l
+  #   ? uyyuebed opponent
+  echo uyyuebed TOV TOV
+  ack --xttfile | wc -l
+  #   ? uyyuebed opponent
+  echo uyyuebed PF PF
+  ack --xttfile | wc -l
+
+done
